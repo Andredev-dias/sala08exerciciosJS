8747595ee649d27dafbf4377afe0d3f5a6381dfd
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-04-28 12:27:51 -0300

    Add files via upload

---
+++ calculos/script.js
@@ -0,0 +1,15 @@
+function somar(){
+    var n1 = document.getElementById("n1").valueAsNumber
+    var n2 = document.getElementById("n2").valueAsNumber 
+    var resp = document.getElementById("resposta")
+    resp.textContent = n1 + n2
+}
+
+function sub(){
+    var n3 = document.getElementById("n3").valueAsNumber
+    var n4 = document.getElementById("n4").valueAsNumber 
+    var resp = document.getElementById("respSub")
+    resp.textContent = n3 - n4
+}
+
+
