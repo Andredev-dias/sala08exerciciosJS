1fe7caf2b8b3db19c792248d6b03d88844059feb
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-04-29 09:27:04 -0300

    Add files via upload

--- calculos/script.js
+++ calculos/script.js
@@ -12,4 +12,23 @@ function sub(){
     resp.textContent = n3 - n4
 }
 
+function multi(){
+    var n3 = document.getElementById("n5").valueAsNumber
+    var n4 = document.getElementById("n6").valueAsNumber 
+    var resp = document.getElementById("respMulti")
+    resp.textContent = n3 * n4
+}
+
+function div(){
+    var n3 = parseFloat(document.getElementById("n7").value)
+    var n4 = parseFloat(document.getElementById("n8").value)
+    var resp = document.getElementById("respDiv")
+    if(n4 !== 0){
+        resp.textContent = n3 / n4
+    }else{
+        resp.textContent = "Não se divide por 0"
+    }
+}
+
+
 
